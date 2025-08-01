326d4ef9bbf4d8afcbd06c4274563617a37c2c60
Alexandre Pereira <pereira.aleandre@gmail.com>
2025-07-31 23:03:25 -0300

    Ajustes do primeiro commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Filament\Panel;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -47,8 +48,9 @@ class User extends Authenticatable
     }
 
 
-    // public function canAccessPanel(Panel $panel): bool
-    // {
-    //     return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
-    // }
+    public function canAccessPanel(Panel $panel): bool
+    {
+      //   return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+      return true;
+    }
 }
